da83223190c83c45760318e49e53d8d68e1b8ee0
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-05-05 17:33:05 +0200

    Squellete form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { Fragment } from "react";
+import Form from "./Components/Form";
+
 function App() {
   return (
     <div className="App">
      <h1 className="text-center mt-3">Todolist</h1>
+     <Form />
     </div>
   );
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Form() {
   return (
-    <div>Form</div>
-  )
+    <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
+      <form className="mb-3">
+        <label htmlFor="todo" className="form-label mt-3">
+          Choses a faire
+        </label>
+        <input type="text" className="form-control" id="todo" />
+        <button className="mt-2 btn btn-primary d-block">Envoyez</button>
+      </form>
+      <h2>Liste des choses a faire :</h2>
+      <ul className="list-group"></ul>
+    </div>
+  );
 }
 
-export default Form
+export default Form;
