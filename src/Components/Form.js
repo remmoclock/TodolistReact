aedfa525c4d04430a996fdb0c5a690ae29574509
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-05-06 16:58:05 +0200

    Add input list

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,20 +1,48 @@
 import React, { useState } from "react";
 import Item from "./Item";
+import { v4 as uuidv4 } from "uuid";
 
 function Form() {
+  const [data, setData] = useState([
+    { txt: "hey", id: uuidv4() },
+    { txt: "yo", id: uuidv4() },
+  ]);
+  const [stateInput, setStateInput] = useState();
+
+  const linkInput = (e) => {
+    setStateInput(e);
+  };
+
+  const addTodo = (e) => {
+    const newArr = [...data];
+    e.preventDefault();
+    const newTodo = {};
+    newTodo.txt = stateInput;
+    newTodo.id = uuidv4();
+    newArr.push(newTodo);
+    setData(newArr);
+  };
+
   return (
     <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
-      <form className="mb-3">
+      <form onSubmit={(e) => addTodo(e)} className="mb-3">
         <label htmlFor="todo" className="form-label mt-3">
           Choses a faire
         </label>
-        <input type="text" className="form-control" id="todo" />
+        <input
+        value={setStateInput}
+          onInput={(e) => linkInput(e.target.value)}
+          type="text"
+          className="form-control"
+          id="todo"
+        />
         <button className="mt-2 btn btn-primary d-block">Envoyez</button>
       </form>
       <h2>Liste des choses a faire :</h2>
       <ul className="list-group">
-        <Item />
-        <Item />
+        {data.map((item, index) => {
+          return <Item txt={item.txt} />;
+        })}
       </ul>
     </div>
   );
